5ca55911ee90760faa5df4f72e70db11cc5ca18c
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2026-07-07 19:45:20 -0600

    loops

--- js-exercises/condicionales.js
+++ js-exercises/condicionales.js
@@ -14,3 +14,49 @@ if (calificacion >= 90){
 }else{
     console.log("Reprobado") // (Falso)
 }
+
+// SWITCH
+
+// Ejercicio opciones de pantalla
+let option = 0;
+switch(option){
+    case 1:
+    case 0:
+        console.log("Maximizar la pantalla")
+        break;
+    case 2:
+        console.log("Minimizar la pantalla")
+        break; // La salida funcional de un block code
+    case 3:
+        console.log("Cerrar la pantalla")
+        break;
+    default:
+        console.log("Opcion no valida, elegir opciones 1-3")
+}
+
+let dia = "Lunes"
+
+switch(dia){
+    case "Lunes":
+        console.log("Hoy es Lunes");
+        break;
+    case "Martes":
+        console.log("Hoy es Martes")
+        break;
+    default:
+        console.log("Hoy es otro dia")
+}
+
+let url = "QA"
+
+switch(url){
+    case "QA":
+    case "QA2":
+        console.log("www.qa.com");
+        break;
+    case "DEV":
+        console.log("www.dev.com")
+        break;
+    default:
+        console.log("Hoy es otro dia")
+}
---
+++ js-exercises/loops.js
@@ -0,0 +1,25 @@
+// LOOPS
+
+// FOR
+for(let i = 1; i<=5; i++){
+    console.log(i)
+}
+
+// WHILE
+let numero = 10
+while(numero >= 1){
+    console.log(numero)
+    //break; // Salida Funcional
+    numero-- // Salida Logica
+}
+
+// DO WHILE
+console.log("----Do While------")
+let doNumero = 1;
+do{
+   console.log(doNumero) 
+   doNumero++
+} while(doNumero <=20)
+
+// Ejercicio en clase: contador del 2 al 20 pero tiene que ser de 2 en 2
+// Ejemplo: 2,4,6,8,10
